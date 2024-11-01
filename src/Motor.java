872ab70a21276809ac4ab4eb91d7fcc7486d576c
vegliraif <veglirafi@gmail.com>
2024-11-01 12:23:34 +0700

    Update Motor.java

--- src/Motor.java
+++ src/Motor.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
 
 public class Motor extends Kendaraan {
-
     public Motor(String platNomor, String merk, String model) {
         super(platNomor, merk, model);
     }
 
-    public static Motor inputMotor() {
-        // try-with-resources -> menutup Scanner secara otomatis
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Masukkan Plat Nomor: ");
-            String platNomor = scanner.nextLine();
-            System.out.print("Masukkan Merk: ");
-            String merk = scanner.nextLine();
-            System.out.print("Masukkan Model: ");
-            String model = scanner.nextLine();
-            // Menghapus input untuk tipeStang
-            return new Motor(platNomor, merk, model);
-        }
+    // Getter dan Setter
+    public static Motor inputMotor(Scanner scanner) {
+        System.out.print("Masukkan Plat Nomor: ");
+        String platNomor = scanner.nextLine();
+        System.out.print("Masukkan Merk: ");
+        String merk = scanner.nextLine();
+        System.out.print("Masukkan Model: ");
+        String model = scanner.nextLine();
+        return new Motor(platNomor, merk, model);
     }
 
     @Override
